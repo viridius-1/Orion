447c919780bed02d1f11bf9e25b4dd0d5ca906cc
Hornet-Developer <hornet.dev.0802@gmail.com>
2020-06-03 14:39:32 -0600

    Added secret key base in application.rb

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Orion
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.secret_key_base = Figaro.env.secret_key_base
   end
 end
