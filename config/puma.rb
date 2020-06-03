705f06810b12240026c4144a7df11e8526b42b99
Hornet-Developer <hornet.dev.0802@gmail.com>
2020-06-03 14:20:42 -0600

    Modified puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,15 @@
+require 'figaro'
+
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
+# and maximum; this matches the default thread size of Active Record.#
+
+Figaro.application = Figaro::Application.new(environment: env, path: "#{root_dir}/config/application.yml")
+Figaro.load
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -16,8 +22,14 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+if environment == 'production'
+  shared_dir = '/home/rails/orion/shared'
+  pidfile "#{shared_dir}/tmp/pids/puma.pid"
+  state_path "#{shared_dir}/tmp/pids/puma.state"
+  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+end
+
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -36,3 +48,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
