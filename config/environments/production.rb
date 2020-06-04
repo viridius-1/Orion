8b686672afb78054fa6ec81a51805f5352f83387
Hornet-Developer <hornet.dev.0802@gmail.com>
2020-06-04 14:17:44 -0600

    Added host to production to fix issue with mail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "orion_production"
 
+  cofig.action_mailer.default_url_options = { host: 'orion.theversion2.com' }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
