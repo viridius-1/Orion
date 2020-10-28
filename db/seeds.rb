a450d5445e1b0c4181111c68123173a7bdfd4356
Hornet-Developer <hornet.dev.0802@gmail.com>
2020-10-28 09:51:55 -0600

    Create admin and user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,30 @@
 
 
 
-# Create default root_admin user
+# Create Agency && Agency's User
+agency = Agency.create!(name: 'First Agency')
 
-User.create!(
-  first_name: 'Shane',
-  last_name: 'Taylor',
-  company: 'N/A',
-  email: 'admin@theversion2.com',
-  password: 'password',
-  confirmed_at: Time.now,
-  roles: :root_admin,
-  user_type: 'agency'
-)
+agency_user = User.create!(first_name: 'First',
+                           last_name: 'Agency',
+                           email: 'first@agency.com',
+                           password: 'password',
+                           confirmed_at: Time.now)
+
+CompanyMember.create!(company_id: agency.id, company_type: agency.class, user_id: agency_user.id)
+
+# Create Advertiser && Advertiser's User
+adv = Advertiser.create!(name: 'First Advertiser')
+
+adv_user = User.create!(first_name: 'First',
+                        last_name: 'Advertiser',
+                        email: 'first@advertiser.com',
+                        password: 'password',
+                        confirmed_at: Time.now)
+
+CompanyMember.create!(company_id: adv.id, company_type: adv_user.class, user_id: adv_user.id)
+
+# Create Interal Admin
+Admin.create!(first_name: 'Admin',
+              last_name: 'Version 2',
+              password: 'overlordorion',
+              email: 'admin@theversion2.com')
