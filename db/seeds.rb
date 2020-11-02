b2287aa1a328435757c7a55396599e18e2622bbc
Hornet-Developer <hornet.dev.0802@gmail.com>
2020-11-02 12:17:15 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,24 +11,24 @@
 # Create Agency && Agency's User
 agency = Agency.create!(name: 'First Agency')
 
-agency_user = User.create!(first_name: '2',
+agency_user = User.create!(first_name: 'First',
                            last_name: 'Agency',
-                           email: '2@agency.com',
+                           email: 'first@agency.com',
                            password: 'password',
                            confirmed_at: Time.now)
 
-CompanyMember.create!(company_id: agency.id, company_type: agency.class, user_id: agency_user.id, roles: 'user')
+CompanyMember.create!(company_id: agency.id, company_type: 'Agency', user_id: agency_user.id, roles: 'user')
 
 # Create Advertiser && Advertiser's User
 adv = Advertiser.create!(name: 'First Advertiser')
 
-adv_user = User.create!(first_name: '2',
+adv_user = User.create!(first_name: 'First',
                         last_name: 'Advertiser',
-                        email: '2@advertiser.com',
+                        email: 'first@advertiser.com',
                         password: 'password',
                         confirmed_at: Time.now)
 
-CompanyMember.create!(company_id: adv.id, company_type: adv_user.class, user_id: adv_user.id, roles: 'user')
+CompanyMember.create!(company_id: adv.id, company_type: 'Advertiser', user_id: adv_user.id, roles: 'user')
 
 # Create Interal Admin
 Admin.create!(first_name: 'Admin',
