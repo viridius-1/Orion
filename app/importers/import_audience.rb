dad20ad8cfab6ed906b962855794bf91020a33f8
Hornet-Developer <hornet.dev.0802@gmail.com>
2020-11-12 14:05:24 -0700

    Create the new audience importer

--- app/importers/import_audience.rb
+++ app/importers/import_audience.rb
@@ -1,14 +1,43 @@
 class ImportAudience
   require 'csv'
 
-  def import(file_name)
-    CSV.foreach("tmp/#{file_name}", headers: true) do |row|
-      row_hash = row.to_h
-      # Audience::Provider
-      # Audience::Category
-      # Audience::Segment
-      segment_description = row_hash['Segment Description']
+  def initialize(file_name)
+    @file_name = file_name
+  end
 
+  def import
+    CSV.foreach("tmp/#{@file_name}", headers: true) do |row|
+      row_hash = {}
+
+      row.to_h.each do |key, value|
+        new_key = key.downcase.split(' ').join('_').to_sym
+        row_hash[new_key] = value
+      end
+
+      provider = Audience::Provider.find_or_create_by!(name: row_hash[:provider])
+
+
+      list_of_audiences = row_hash[:segment_name].split(' > ').drop(1)
+
+      # Some audiences has 1 layer to pull from
+      # This will recreate the 2nd layer for Audience Catgeory and Segment
+      list_of_audiences = list_of_audiences * 2 if list_of_audiences.count <= 1
+
+      segment_name = list_of_audiences.pop
+
+
+      parent_category = nil
+
+      list_of_audiences.map do |name|
+        parent_category = Audience::Category.find_or_create_by!(name: name,
+                                                                category_id: parent_category&.id,
+                                                                provider_id: provider&.id)
+
+      end
+
+      segment = Audience::Segment.find_or_create_by!(name: segment_name,
+                                                     description: row_hash[:segment_description],
+                                                     category_id: parent_category&.id)
     end
   end
 end
--- app/models/audience/segment.rb
+++ app/models/audience/segment.rb
@@ -1,5 +1,5 @@
 class Audience::Segment < ApplicationRecord
-  belongs_to :category, class_name: 'Audience::Category'
+  belongs_to :category, class_name: 'Audience::Category', optional: true
   has_one :provider, class_name: 'Audience::Provider', through: :category, dependent: :destroy
 
   validates :name, presence: true
