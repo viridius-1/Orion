c04615bc3ed295e9a68a13c48512cf541f8d25a4
Hornet-Developer <hornet.dev.0802@gmail.com>
2021-03-06 18:00:57 -0700

    Create and update campaign basic and campaign form logic

--- app/javascript/bundles/Campaign/New.jsx
+++ app/javascript/bundles/Campaign/New.jsx
@@ -1,44 +1,31 @@
-import React, { Component, Fragment } from "react";
-import NumericInput from "react-numeric-input";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import moment from "moment";
+import React, { Component } from "react";
 
-import AudienceFields from "./components/AudienceFields";
-import CreateCampaign from "./components/CreateCampaign";
-import { showErrorStyles, showLabel } from "./common/";
+import CampaignBasic from "./components/CampaignBasic";
 
 export default class New extends Component {
   constructor(props) {
     super(props);
 
-    const { campaign, campaign_audiences } = this.props;
-
-    const {
-      name,
-      url,
-      goal,
-      kpi,
-      cpa_goal,
-      roas_goal,
-      budget,
-      geography,
-      flight_start_date,
-      flight_end_date,
-    } = campaign;
-
     this.state = {
-      name: name || "",
-      url: url || "",
-      goal: goal || "",
-      kpi: kpi || "",
-      cpa_goal: cpa_goal || "",
-      roas_goal: roas_goal || "",
-      budget: budget || "",
-      geography: geography || "",
-      flight_start_date: new Date(flight_start_date) || new Date(),
-      flight_end_date: new Date(flight_end_date) || new Date(),
-      audiences: campaign_audiences || [],
+      step: 0,
+      name: "",
+      url: "",
+      goal: "",
+      kpi: "",
+      cpa_goal: "",
+      roas_goal: "",
+      budget: "",
+      geography: "",
+      flight_start_date: null,
+      flight_end_date: null,
+      audiences: [],
+      coversion_rate: "",
+      aov: "",
+      gender: "",
+      age_range: "",
+      education: "",
+      parental_status: "",
+      data_providers: "",
       errors: {
         name: "",
         url: "",
@@ -50,6 +37,13 @@ export default class New extends Component {
         geography: "",
         flight_start_date: "",
         flight_end_date: "",
+        coversion_rate: "",
+        aov: "",
+        gender: "",
+        age_range: "",
+        education: "",
+        parental_status: "",
+        data_providers: "",
       },
     };
   }
@@ -64,140 +58,109 @@ export default class New extends Component {
     this.setState({ errors, [name]: value });
   };
 
+  nextStep = () => {
+    const { step } = this.state;
+
+    this.setState({
+      step: step + 1,
+    });
+  };
+
+  prevStep = () => {
+    const { step } = this.state;
+
+    this.setState({
+      step: step - 1,
+    });
+  };
+
+  redirectTo = () => {
+    const { company } = this.props;
+    const url = `/agencies/${company.agency_id}/clients/${company.id}/campaigns`;
+
+    window.location.assign(url);
+  };
+
   handleDateNumSelect = (key, event) => {
     this.setState({ [key]: event });
   };
 
-  showInput(key, value, errors, goals, kpis) {
-    if (key == "cpa_goal" || key == "budget" || key == "roas_goal") {
-      return (
-        <NumericInput
-          className={showErrorStyles(errors[key])}
-          name={`${key}`}
-          min={1}
-          onChange={(event) => this.handleDateNumSelect(key, event)}
-          value={value}
-        />
-      );
-    } else if (key == "flight_start_date" || key == "flight_end_date") {
-      return (
-        <DatePicker
-          className={showErrorStyles(errors[key])}
-          name={`${key}`}
-          onSelect={(event) => this.handleDateNumSelect(key, event)}
-          selected={value}
-        />
-      );
-    } else if (key == "goal" || key == "kpi") {
-      const obj = key == "goal" ? goals : kpis;
+  handleCreationFlow(state) {
+    const { step, data_providers, ...fields } = this.state;
 
+    const {
+      errors,
+      name,
+      url,
+      flight_start_date,
+      flight_end_date,
+      goal,
+      kpi,
+      cpa_goal,
+      roas_goal,
+      budget,
+      geography,
+      coversion_rate,
+      aov,
+      gender,
+      age_range,
+      education,
+      parental_status,
+    } = fields;
+
+    if (step == 0) {
       return (
-        <select
-          className={showErrorStyles(errors[key])}
-          name={`${key}`}
-          onSelect={this.handleInputChange}
-        >
-          {obj.map((goal) => {
-            return <option>{goal}</option>;
-          })}
-        </select>
+        <CampaignBasic
+          fields={{ errors, name, url, flight_start_date, flight_end_date }}
+          currentStep={step}
+          prevStep={this.prevStep}
+          nextStep={this.nextStep}
+          handleDateNumSelect={this.handleDateNumSelect}
+          handleInputChange={this.handleInputChange}
+        />
       );
     } else {
       return (
-        <input
-          className={showErrorStyles(errors[key])}
-          name={`${key}`}
-          onChange={this.handleInputChange}
-          value={value}
-        />
+        <DataProvider dataProviders={data_providers} prevStep={this.prevStep} />
       );
     }
   }
-
-  showError(key, errors) {
-    if (key == "name") {
-      return (
-        errors.name.length > 0 && (
-          <span className="error-msg">{errors.name}</span>
-        )
-      );
-    }
-  }
-
-  showCampaignForm(formFields, goal, kpi) {
-    const { errors, audiences, ...fields } = formFields;
-
-    return Object.entries(fields).map(([key, value]) => {
-      return (
-        <div className="col col-6">
-          <div className={`form-group ${key}`} key={key}>
-            <label key={key}>{showLabel(key)}</label>
-            {this.showInput(key, value, errors, goal, kpi)}
-            {this.showError(key, errors)}
-          </div>
-        </div>
-      );
-    });
-  }
-
-  setAudienceState = (data) => {
-    event.preventDefault();
-
-    this.setState({ audiences: data });
-  };
-
-  setErrorMessages = (errors) => {
-    this.setState({ errors });
-  };
 
   render() {
-    const {
-      page_type,
-      company,
-      is_client,
-      goal_options,
-      kpi_options,
-      audiences,
-      campaign,
-      campaign_audiences,
-    } = this.props;
+    const { page_type } = this.props;
+    const { step } = this.state;
 
     return (
-      <div className="container-fluid campaigns">
+      <div className="container campaigns">
         <div className="row">
           <div className="col col-12">
-            <h1 className="h5 mb-4">
-              {page_type == "Edit" ? "Edit Campaign" : "Create Campaign"}
+            <h1 className="h3 mb-4">
+              {page_type == "Edit" ? "Edit Campaign" : "Campaign Setup"}
             </h1>
-            <div className="card">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col col-12">
-                    <p className="light-text">
-                      Please enter the details of your new campaign
-                    </p>
+            <div className="card campaign-card">
+              <div className="card-body d-flex flex-column campaign-card-body">
+                {/* Render step comp here */}
+
+                <hr></hr>
+
+                {this.handleCreationFlow(this.state)}
+
+                <div className="row mt-5">
+                  <div className="col-6 d-flex justify-content-between btn-section">
+                    <div
+                      className="btn-lg d-flex align-items-center justify-content-center back-btn"
+                      onClick={step == 0 ? this.redirectTo : this.prevStep}
+                    >
+                      Cancel
+                    </div>
+                    <div
+                      className="btn-lg d-flex align-items-center justify-content-center next-btn"
+                      onClick={this.nextStep}
+                    >
+                      Continue to Goal
+                    </div>
                   </div>
                 </div>
-                <form>
-                  <div className="row">
-                    {this.showCampaignForm(
-                      this.state,
-                      goal_options,
-                      kpi_options
-                    )}
-                    <AudienceFields
-                      audiences={audiences}
-                      audienceState={this.state.audiences}
-                      setAudienceState={this.setAudienceState}
-                    />
-                    <CreateCampaign
-                      company={company}
-                      isClient={is_client}
-                      stateProps={this.state}
-                      setErrorMessages={this.setErrorMessages}
-                    />
-                  </div>
-                </form>
               </div>
             </div>
           </div>
---
+++ app/javascript/bundles/Campaign/components/DataProvider.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class DataProvider extends Component {
+  render() {
+    return <div></div>;
+  }
+}
