4ad5272b7eede358a934b568553f733c213edce2
Hornet-Developer <hornet.dev.0802@gmail.com>
2021-03-15 14:04:13 -0600

    Update campaign helper

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,28 +1,29 @@
 module CampaignsHelper
   def goal_options
-    [
-      'Reach',
-      'Awareness',
-      'Acquisition',
-      'TBD'
-    ]
+    ['Reach',
+     'Awareness',
+     'Acquisition',
+     'TBD']
   end
 
   def kpi_options
-    [
-      'Impressions',
-      'Click Through Rate (CTR)',
-      'Cost Per Acquisition (CPA)',
-      'Video Completion Rate',
-      'TBD'
-    ]
+    ['Impressions',
+     'Click Through Rate (CTR)',
+     'Cost Per Acquisition (CPA)',
+     'Video Completion Rate',
+     'TBD']
   end
 
   def education_options
-    ['Highschool']
+    ['Less than high school graduate',
+     'High school graduate',
+     'Some college or associates degree',
+     'Bachelor’s degree',
+     'Graduate or professional degree']
   end
 
   def parental_options
-    ['Single']
+    ['Parent',
+     'Not Parent']
   end
 end
