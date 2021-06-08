77e27512a5aaf412bbc951eabef662752ba6357b
Hornet-Developer <hornet.dev.0802@gmail.com>
2021-06-07 22:12:54 -0400

    Fix admin sign in page from UI

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Admins::SessionsController < Devise::SessionsController
+  before_action :authorize_admin, only: [:new]
+
+  def new
+    super
+  end
+
+  private
+
+  def authorize_admin
+    redirect_to root_path if user_signed_in?
+  end
 end
