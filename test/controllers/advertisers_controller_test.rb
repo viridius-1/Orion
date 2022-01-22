537b8ab9fb15fe3744569ec639bae0f041ae14bc
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-01-22 14:22:51 +0100

    initial tests setup, added advertiser and camaign controller tests

--- test/controllers/advertisers_controller_test.rb
+++ test/controllers/advertisers_controller_test.rb
@@ -1,48 +1,198 @@
 require 'test_helper'
 
 class AdvertisersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @advertiser = advertisers(:one)
+  include Devise::Test::IntegrationHelpers
+
+  # INDEX
+
+  test 'index redirects when not logged' do
+    get "/vendors?agency_id=#{agencies(:first).id}"
+
+    assert_response :redirect
   end
 
-  test "should get index" do
-    get advertisers_url
+  test 'index redirects when agency does not belong to current user' do
+    sign_in users(:advertiser_user)
+
+    get "/vendors?agency_id=#{agencies(:first).id}"
+
+    assert_response :redirect
+  end
+
+  test 'should get index when logged in as agency_user' do
+    sign_in users(:agency_user)
+
+    get "/vendors?agency_id=#{agencies(:first).id}"
+
     assert_response :success
+    assert_equal assigns(:agency), agencies(:first)
   end
 
-  test "should get new" do
-    get new_advertiser_url
+  # NEW
+
+  test 'new redirects when not logged in' do
+    get "/vendors?agency_id=#{agencies(:first).id}"
+
+    assert_response :redirect
+  end
+
+  test 'new redirects to when agency does not belong to current user' do
+    sign_in(:advertiser_user)
+
+    get "/vendors?agency_id=#{agencies(:first).id}"
+
+    assert_response :redirect
+  end
+
+  test 'should get new' do
+    sign_in users(:agency_user)
+
+    get "/vendors/new?agency_id=#{agencies(:first).id}"
+
     assert_response :success
+    assert_equal assigns(:agency), agencies(:first)
   end
 
-  test "should create advertiser" do
+  # CREATE
+
+  test 'create redirects when not logged in' do
+    post "/vendors?agency_id=#{agencies(:first).id}"
+
+    assert_response :redirect
+  end
+
+  test 'create redirects when agency does not belong to current user' do
+    sign_in users(:advertiser_user)
+
+    post "/vendors?agency_id=#{agencies(:first).id}"
+
+    assert_response :redirect
+  end
+
+  test 'should create advertiser as agency user' do
+    sign_in users(:agency_user)
+
     assert_difference('Advertiser.count') do
-      post advertisers_url, params: { advertiser: { name: @advertiser.name, logo_url: @advertiser.logo_url, user_id: @advertiser.user_id, website: @advertiser.website } }
+      post "/vendors?agency_id=#{agencies(:first).id}",
+           params: {
+             advertiser: {
+               annual_revenue: 50_000_000,
+               name: "Example Adworks",
+               webiste_url: "www.example.com",
+               industry: "Agribusiness",
+               current_media_mix: ["Connected TV"],
+               business_type: "Business to Business",
+               monthly_unique_visitors: 40_000
+            }
+          }
     end
 
-    assert_redirected_to advertiser_url(Advertiser.last)
+    assert_redirected_to agency_vendors_path(agencies(:first).id)
   end
 
-  test "should show advertiser" do
-    get advertiser_url(@advertiser)
+  # EDIT
+
+  test 'edit should redirect if user is not logged in' do
+    get "/vendors/#{advertisers(:first).id}/edit"
+
+    assert_response :redirect
+  end
+
+  test 'edit should redirect if user does not have access to advertiser' do
+    sign_in users(:no_access_user)
+
+    get "/vendors/#{advertisers(:first).id}/edit"
+
+    assert_response :redirect
+  end
+
+  test 'should get edit as agency user' do
+    sign_in users(:agency_user)
+
+    get "/vendors/#{advertisers(:first).id}/edit"
+
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_advertiser_url(@advertiser)
+  test 'should get edit as advertiser user' do
+    sign_in users(:advertiser_user)
+
+    get "/vendors/#{advertisers(:first).id}/edit"
+
     assert_response :success
   end
 
-  test "should update advertiser" do
-    patch advertiser_url(@advertiser), params: { advertiser: { name: @advertiser.name, logo_url: @advertiser.logo_url, user_id: @advertiser.user_id, website: @advertiser.website } }
-    assert_redirected_to advertiser_url(@advertiser)
+  # UPDATE
+
+  test 'update should redirect if user is not logged in' do
+    patch "/vendors/#{advertisers(:first).id}?agency_id=#{agencies(:first).id}",
+          params: { advertiser: { name: "Updated Adworks" } }
+
+    assert_response :redirect
+    assert_equal advertisers(:first).reload.name, "Adworks"
   end
 
-  test "should destroy advertiser" do
+  test 'update should redirect if user does not have access to advertiser' do
+    sign_in users(:no_access_user)
+
+    patch "/vendors/#{advertisers(:first).id}?agency_id=#{agencies(:first).id}",
+          params: { advertiser: { name: "Updated Adworks" } }
+
+    assert_response :redirect
+    assert_equal advertisers(:first).reload.name, "Adworks"
+  end
+
+  test 'should update advertiser' do
+    sign_in users(:advertiser_user)
+
+    patch "/vendors/#{advertisers(:first).id}?agency_id=#{agencies(:first).id}",
+          params: { advertiser: { name: "Updated Adworks" } }
+          
+    assert_redirected_to agency_vendors_path(agencies(:first).id)
+    assert_equal advertisers(:first).reload.name, "Updated Adworks"
+  end
+
+  # TODO - validation error case doesn't work for update action
+
+  # test "should redirect to edit if update fails" do
+  #   sign_in users(:advertiser_user)
+
+  #   advertiser_params = { advertiser: { name: nil } }
+
+  #   patch "/vendors/#{advertisers(:first).id}?agency_id=#{agencies(:first).id}",
+  #         params: advertiser_params
+
+  #   assert_redirected_to agency_vendors_path(agencies(:first).id)
+  # end
+
+  # DESTROY
+
+  test 'destroy redirects if user is not logged in' do
+    assert_no_changes('Advertiser.count', -1) do
+      delete "/vendors/#{advertisers(:first).id}?agency_id=#{agencies(:first).id}"
+    end
+
+    assert_response :redirect
+  end
+
+  test 'destroy redirects if user does not have access to agency' do
+    sign_in users(:advertiser_user)
+
+    assert_no_changes('Advertiser.count', -1) do
+      delete "/vendors/#{advertisers(:first).id}?agency_id=#{agencies(:first).id}"
+    end
+
+    assert_response :redirect
+  end
+
+  test 'should destroy advertiser' do
+    sign_in users(:agency_user)
+
     assert_difference('Advertiser.count', -1) do
-      delete advertiser_url(@advertiser)
+      delete "/vendors/#{advertisers(:first).id}?agency_id=#{agencies(:first).id}"
     end
 
-    assert_redirected_to advertisers_url
+    assert_equal assigns(:agency), agencies(:first)
+    assert_redirected_to agency_vendors_path(agencies(:first).id)
   end
 end
--- test/models/audience/category_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Audience::CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
