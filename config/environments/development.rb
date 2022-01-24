c1c4d0220fe715292077d7a44bcdd19d53ddc963
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-01-24 20:57:33 +0100

    Added welcome mailer and letter opener for testing mails locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Use letter opener to see preview of emails in browser
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
