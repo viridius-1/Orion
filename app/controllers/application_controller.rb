c162b3ae57c3c63a0ca2ba8e7c35afa99d46ee18
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-02-14 15:09:18 +0100

    moved logout to settings (#88)
    * moved logout to settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,8 @@ class ApplicationController < ActionController::Base
       accept_user_invitation_path
     end
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
